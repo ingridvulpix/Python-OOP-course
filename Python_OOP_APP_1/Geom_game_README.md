bdd155849bb9230c0e1e170318cbfd70eb7b7272
ingridvulpix <ingridalmeidafs@gmail.com>
2022-06-23 21:12:12 -0300

    class area

--- Python_OOP_APP_1/Geom_game_README.md
+++ Python_OOP_APP_1/Geom_game_README.md
@@ -4,6 +4,9 @@ Geometry Game!!
 - Show it's coordinates
 - The user insert the coordinates of a point
 - Check if the point is inside of the rectangle or not
+- The user insert a guess of the rectangle area
+- Check if is right or not
+- If isn't right, it shows the right answer
 
 --> Terminal output should be:
 
@@ -12,3 +15,11 @@ Guess point x coord: [user_guess_x]
 Guess point y coord: [user_guess_y]
 
 Your point was inside rectangle: [bool]
+
+Guess the rectangle area: [user_guess_area]
+
+Your guess was right: [bool]
+
+-If bool == False
+
+The right area is: [rectangle_area]
--- Python_OOP_APP_1/Geometry Game.py
+++ Python_OOP_APP_1/Geometry Game.py
@@ -6,8 +6,8 @@ class Point:
         self.y = y
     
     def inside_rectangle(self, rec):
-        if rec.lowleft.x < self.x < rec.upright.x \
-            and rec.lowleft.y < self.y < rec.upright.y:
+        if rec.lowleft.x <= self.x <= rec.upright.x \
+            and rec.lowleft.y <= self.y <= rec.upright.y:
             return True
         else:
             return False
@@ -17,7 +17,27 @@ class Rectangle:
     def __init__(self, lowleft, upright):
         self.lowleft = lowleft
         self.upright = upright
+        self.base = abs(self.lowleft.x - self.upright.x)
+        self.heigh = abs(self.lowleft.y - self.upright.y)
 
+
+
+class Area(Rectangle):
+    def __init__(self, rec):
+        super().__init__(rec.lowleft,rec.upright)
+    
+    def calculate_area(self):
+        self.area = float(self.base * self.heigh)
+
+    def show_area (self):
+        return str(self.area)
+
+    def guess_area(self, guess):
+        self.calculate_area()
+        if self.area == guess:
+            return True
+        else:
+            return False
     
 if __name__ == '__main__':
 
@@ -35,3 +55,10 @@ if __name__ == '__main__':
     print(f'\nYour point was inside rectangle: {user_point.inside_rectangle(rectangle)}')
 
 
+    user_area = Area(rectangle)
+    guess = user_area.guess_area(float(input('Guess the rectangle area: ')))
+
+    print(f'Your guessed area was right: {guess}')
+    
+    if guess == False:
+        print(f'The right area is: {user_area.show_area()}')
